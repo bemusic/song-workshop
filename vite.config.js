69c24053de24e924956aec4198b27eec8ae9f015
dtinth on MBP M1 <dtinth@spacet.me>
2021-10-22 22:44:37 +0700

    Remove headers plugin

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,7 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-const headers = (headers) => ({
-  name: "headers",
-  configureServer(server) {
-    server.middlewares.use((req, res, next) => {
-      for (const [key, value] of Object.entries(headers)) {
-        res.setHeader(key, value);
-      }
-      next();
-    });
-  },
-});
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    svelte(),
-    headers({
-      "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Opener-Policy": "same-origin",
-    }),
-  ],
+  plugins: [svelte()],
 });
