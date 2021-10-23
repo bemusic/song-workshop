5d2a50945f6a1e91b4de6ca56c25b85f4a81bafb
dtinth on MBP M1 <dtinth@spacet.me>
2021-10-23 11:25:04 +0700

    Add Markdown component

---
+++ src/Markdown.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Markdown from "markdown-it";
+
+  const markdown = new Markdown({
+    linkify: true,
+    breaks: true,
+    typographer: true,
+  });
+
+  export let source: string;
+
+  $: html = markdown.render(source);
+</script>
+
+{@html html}
